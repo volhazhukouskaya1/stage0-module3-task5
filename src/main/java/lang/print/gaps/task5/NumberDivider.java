ccea776325ceaf37b9e7c8a9b629d93b77eeb8d5
volhazhukouskaya1 <volha_zhukouskaya@outlook.com>
2024-04-29 21:34:56 +0300

    task5 -2

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-    float divided = toBeDivided/5;
+    double divided = (double)(toBeDivided)/5;
     System.out.println(divided);}
 }
